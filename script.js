1b0c5c603e83adccd673e45e829559c39fefd65c
Sacha <s.hauberdon@gmail.com>
2022-01-11 14:52:19 +0100

    project ok

--- script.js
+++ script.js
@@ -1,57 +1,37 @@
-var messagesCount = document.getElementsByTagName('p').length;
-document.getElementById('count').textContent = messagesCount;
+let messagesCount = $("p").length;
+$("#count").text(messagesCount);
 
-for(var i=0; i<document.getElementsByClassName('trash').length; i++){
-    document.getElementsByClassName('trash')[i].addEventListener("click",
-    function(){
-        this.parentNode.remove();
-        var messagesCount = document.getElementsByTagName('p').length;
-        document.getElementById('count').textContent = messagesCount;
+$("body").on("click", ".trash", function () {
+  $(this).parent().remove();
+  messagesCount--;
+  $("#count").text(messagesCount);
+});
+
+$("#btn-add").click(function () {
+  let msg = $("#add-message").val();
+  $("body").append(
+    ` 
+        <div class='row'>
+        <img class='avatar' src='./img/avatar-2.jpg'>
+        <div class='content'>
+        <h6>Sacha Hauberdon</h6>
+        <p>` +
+      msg +
+      `</p>
+        </div>
+        <img class='trash' src='./img/trash.png'>
+        </div>
+    `
+  );
+  messagesCount++;
+  $("#count").text(messagesCount);
+  $("#add-message").val("");
+});
+
+$("#btn-search").click(function () {
+  $("h6").each(function () {
+    if ($("#search-bar").val() != $(this).text()) {
+      $(this).parent().parent().fadeOut();
     }
-    )
-
-}
-
-document.getElementById('btn-add').addEventListener("click",
-    function(){
-        var mainDiv = document.createElement("div");
-            mainDiv.className = "row";
-            document.body.appendChild(mainDiv);
-
-        var image = document.createElement("img");
-            image.className = "avatar";
-            image.src = "avatar-1.jpg";
-            mainDiv.appendChild(image);
-        
-        var nameMessageDiv = document.createElement("div");
-            mainDiv.appendChild(nameMessageDiv);
-
-        var h6 = document.createElement('h6');
-            h6.textContent = "Eric Dupont";
-            nameMessageDiv.appendChild(h6);
-
-        var content = document.createElement('p');
-            content.textContent = document.getElementById('add-message').value;
-            nameMessageDiv.appendChild(content);
-
-        var trash = document.createElement('img');
-        trash.src = "trash.png";
-        trash.className = "trash";
-        mainDiv.appendChild(trash);
-
-        document.getElementById('add-message').value = "";
-        
-        var messagesCount = document.getElementsByTagName('p').length;
-        document.getElementById('count').textContent = messagesCount;
-
-        
-        trash.addEventListener("click", 
-            function(){
-                this.parentNode.remove();
-                var messagesCount = document.getElementsByTagName('p').length;
-                document.getElementById('count').textContent = messagesCount;
-
-            }
-        )
-    }
-)
+  });
+});
